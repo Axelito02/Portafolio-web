7237adb11be28905356c4b0f0bf950a88e5d0f53
Axelito02 <98567345+Axelito02@users.noreply.github.com>
2025-04-30 22:47:04 -0500

    fixed deploy

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('app')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/Portafolio-web/'>
     <App />
   </BrowserRouter>,
 )
