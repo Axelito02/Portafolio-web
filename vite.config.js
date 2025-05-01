1a8fb958624aaacf5e7d8d2b5de41b9e8a6e7097
Axelito02 <98567345+Axelito02@users.noreply.github.com>
2025-04-30 20:27:56 -0500

    fixed deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/PaginaWeb"
+  base: "https://axelito02.github.io/Portafolio-web/"
 })
